1b50a8986e75912c297f84d369e229bd6c72e4ba
LucianaPB92 <lucianapbarrionuevo@hotmail.com>
2024-03-31 20:32:45 -0300

    agregue carpeta clase1-javascript con archivo index y js

---
+++ clase1-javascript/js/app.js
@@ -0,0 +1,67 @@
+//  alert("Hola estamos en el frontend");
+
+/*alert(5+17);
+console.log(14-25);
+console.log(23>50);
+let nombre = "Luis";
+console.log(nombre);
+nombre =53; 
+console.log(nombre);*/
+
+//variables
+//Declaracion palabra reservada let nombre-variable signo de asignacion valor
+//2da forma de declarar variable
+let nombre;
+//asignamos valor
+nombre ="Luis";
+//numerica
+//Declaracion e inicializacion
+let numero = 10.50;
+//booleano
+let llueve =false;
+let esta_conectada =true;
+//Uso de la variable
+console.log(nombre);
+
+//
+nombre ="Ángel";
+console.log(nombre);
+
+let base = 10;
+let altura = 5;
+let resultado =base*altura;
+console.log("el area del rectangulo es", resultado);
+
+//tipos de variables
+//string
+let miNombre ="Juan"
+//numerico
+let num=100000;
+let iva;
+
+resultado=num*21/100;
+let resultado2=num*0.21;
+
+console.log("El iva de "+num+" es: "+resultado);
+console.log(typeof resultado);
+console.log(typeof nombre);
+console.log(typeof operacion);
+
+let op;
+op=true;
+let alumno={}
+let alumnos=[];
+console.log(typeof alumno);
+console.log(typeof alumnos);
+
+let aula=10;
+console.log(aula);
+aula=23;
+console.log(aula);
+
+const PI = 3.14;
+let r = 15;
+
+resultado = r*r*PI;
+
+console.log("El área del circulo es: " +resultado);
